dd648e78deb576ee48bc92d1ceb26174e141a679
Takuji-Ogura <ogura@secureinc.co.jp>
2022-05-12 17:53:05 +0900

    test commit

--- GithubActionTest/Form1.cs
+++ GithubActionTest/Form1.cs
@@ -16,5 +16,10 @@ namespace GithubActionTest
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
